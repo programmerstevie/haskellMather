7b25687138488ed977f544125ce226c9f6cf2121
Steven Santos <stevensan0606@gmail.com>
2021-03-25 06:53:51 -0400

    formatting

--- src/Token.hs
+++ src/Token.hs
@@ -3,9 +3,10 @@ module Token where
 
 import Grammar
 
-data Token = ConstI_T Integer
-           | ConstD_T Double
-           | Binop_T Binop
-           | LParen_T 
-           | RParen_T
-           deriving (Show, Eq)
+data Token
+  = ConstI_T Integer
+  | ConstD_T Double
+  | Binop_T Binop
+  | LParen_T
+  | RParen_T
+  deriving (Show, Eq)
